cbee79819d115e0934458d8b38a529a778d47114
Neala Mendoza <mendo151@cougars.csusm.edu>
2021-01-09 13:38:22 -0800

    made firebaswe.js and signup.js

---
+++ static/firebase.js
@@ -0,0 +1,23 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.2.2/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.2.2/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyArKV4iGxuf1mH0VSkYtP4di81ZbwbMjAM",
+    authDomain: "cardfolio-35477.firebaseapp.com",
+    projectId: "cardfolio-35477",
+    storageBucket: "cardfolio-35477.appspot.com",
+    messagingSenderId: "719655617040",
+    appId: "1:719655617040:web:d5ff270d9b964cd10dc095",
+    measurementId: "G-QVXK23DCQ3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
---
+++ static/signup.js
@@ -0,0 +1,29 @@
+function createUser() {
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((user) => {
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        user.sendEmailVerification().then(function() {
+          // Email sent.
+          location.href = '/';
+        }).catch(function(error) {
+          // An error happened.
+        });
+        console.log(user);
+      } else {
+        console.log("user is not signed in");
+      }
+    });
+
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+    console.log(errorMessage);
+    console.log(user);
+  //  console.log(user.isEmailVerified());
+  });
+}
