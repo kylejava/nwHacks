121cb404d8b3a64abf268096e9f79e10cfcddfb3
kylejava <kyleedwardgjava@gmail.com>
2021-01-09 14:34:16 -0800

    Still need to create templates and work on backend

--- app.py
+++ app.py
@@ -5,5 +5,25 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/getstarted' , methods = ['GET' , 'POST'])
+def getstarted():
+    return render_template('getstarted.html')
+
+@app.route('/signin' , methods = ['GET' , 'POST'])
+def signin():
+    return render_template('signin.html')
+
+@app.route('/signup' , methods = ['GET' , 'POST'])
+def signup():
+    return render_template('signup.html')
+
+@app.route('/templates' , methods = ['GET' , 'POST'])
+def teplates():
+    return render_template('templates.html')
+
+@app.route('/userinput' , methods = ['GET' , 'POST'])
+def userinput():
+    return render_template('userinput.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
