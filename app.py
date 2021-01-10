6c1811b49f54075c90b0ee3194cd560bf0d68ad6
kylejava <kyleedwardgjava@gmail.com>
2021-01-10 00:09:55 -0800

    Fixing up designs

--- app.py
+++ app.py
@@ -23,15 +23,29 @@ def teplates():
 
 @app.route('/userinput' , methods = ['GET' , 'POST'])
 def userinput():
+    name = ""
+    email = ""
+    phonenumber = ""
+    address = ""
+    position = ""
+    website = ""
+    templateSelection = ""
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phonenumber = request.form['phonenumber']
+        address = request.form['address']
+        position = request.form['position']
+        website = request.form['website']
+        templateSelection = request.form.get('template-selection')
+        if(templateSelection == 'template-one'):
+            return render_template('template1.html', name= name , email = email, phonenumber= phonenumber,address = address, position= position, website = website)
+        elif(templateSelection == 'template-two'):
+            return render_template('template2.html', name= name , email = email, phonenumber= phonenumber,address = address, position= position, website = website)
+        elif(templateSelection == 'template-three'):
+            return render_template('template3.html', name= name , email = email, phonenumber= phonenumber,address = address, position= position, website = website)
+            
     return render_template('userinput.html')
-
-@app.route('/template1' , methods = ['GET' , 'POST'])
-def template1():
-    return render_template('template1.html')
-
-@app.route('/template2' , methods = ['GET' , 'POST'])
-def template2():
-    return render_template('template3.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
